b507b75b13867d64a466f29dd71d3c5858606f68
alexjoshva <alexchandarjoshva@gmail.com>
2026-05-07 21:46:25 +0530

    feat: initialize global CSS styles and add Features page structure

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -55,8 +55,11 @@ export default function Features() {
                     <div className="core-ring cr-2"></div>
                   </div>
                   <div className="core-center">
-                    <span className="core-logo">RC</span>
-                    <span className="core-text">Sync Engine</span>
+                    <span className="core-logo">
+                      <svg width="40" height="40" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/>
+                      </svg>
+                    </span>
                   </div>
                 </div>
               </div>
